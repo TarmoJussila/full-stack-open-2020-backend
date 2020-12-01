02de51b68ae8179543642053fd6d27fdfb81c378
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-12-01 23:04:25 +0200

    3.12: phonebook database WIP.
    Using Mongo DB Atlas.

---
+++ mongo.js
@@ -0,0 +1,47 @@
+// mongodb+srv://fullstack:<password>@fullstack-cluster.axkrj.mongodb.net/<dbname>?retryWrites=true&w=majority
+// Replace <password> with the password for the fullstack user.
+// Replace <dbname> with the name of the database that connections will use by default.
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Provide password as argument!')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@fullstack-cluster.axkrj.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log(`Phonebook (${result.length}):`)
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+else if (process.argv.length === 5) {
+  const person = new Person({
+    name: 'Test Person 2',
+    number: '123-456-7689 0'
+  })
+  
+  person.save().then(result => {
+    console.log(`Succesfully added ${result.name} ${result.number} to the phonebook database.`)
+    mongoose.connection.close()
+  })
+}
