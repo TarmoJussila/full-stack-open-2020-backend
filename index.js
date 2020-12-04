c92e04c2f5b9f02edbe17725b6ae4fddbe21582a
Tarmo Jussila <tarmo.jussila@zaibatsu.fi>
2020-12-04 19:28:05 +0200

    Part 3.17: phonebook and backend.
    Person number can be updated by using the same name when inserting new person info.

--- index.js
+++ index.js
@@ -83,6 +83,20 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    number: body.number.trim()
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'Unknown endpoint!' })
 }
